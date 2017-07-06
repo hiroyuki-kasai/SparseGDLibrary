3225044a0cf9042b511432bf8738e2f0e17ea98c
Hiroyuki KASAI <kasai@HiroyukinoiMac.local>
2017-07-07 08:14:46 +0900

    Version 1.0.2
    Modified.

--- tool/svd_shrink.m
+++ tool/svd_shrink.m
@@ -1,9 +1,17 @@
 function [ v ] = svd_shrink(w, t, dim)
-    L = reshape(w, dim);
-    [U,S,V] = svd(L,'econ');
+    if nargin == 3
+        L = reshape(w, dim);
+    else
+        L = w;
+    end
+    [U,S,V] = svd(L, 'econ');
     s = diag(S);
     S = diag(sign(s) .* max(abs(s) - t,0));
     L = U*S*V';
-    v = L(:);
+    if nargin == 3
+        v = L(:);
+    else
+        v = L;
+    end
 end
 
